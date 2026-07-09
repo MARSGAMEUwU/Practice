1721ffae856cbef53482bdaae66bafa18554baf8
mynti <artem_mkarimov@mail.ru>
2026-07-09 09:49:41 +0300

    ацайц

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -3,6 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class GameOverManager : MonoBehaviour
 {
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+
+        // Делаем курсор видимым
+        Cursor.visible = true;
+    }
+
     public void RestartGame()
     {
         SceneManager.LoadScene("MainMenu");
